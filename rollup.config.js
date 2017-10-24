2579e8bd1cea7c45af357f6e56d8866768bc4009
Denis <carriere.denis@gmail.com>
2017-10-24 01:20:00 -0400

    Simplify rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,3 @@
-// https://github.com/Turfjs/turf/pull/986
-function typescriptExport () {
-  return {
-    name: 'typescript-export',
-    transformBundle (code) {
-      code = code.trim()
-      const name = code.match(/module.exports = (\w+)/)
-      if (name) code += `\nmodule.exports.default = ${name[1]};\n`
-      return code
-    }
-  }
-}
-
 export default {
   input: 'index.js',
   output: {
@@ -18,5 +5,5 @@ export default {
     file: 'main.js',
     format: 'cjs'
   },
-  plugins: [typescriptExport()]
+  plugins: []
 }
